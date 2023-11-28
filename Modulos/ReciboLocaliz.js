4cda47483fc7229dc5f5dcbbc9974a8fbda5ba48
Igor Xavier <145226720+IgorXRS@users.noreply.github.com>
2023-11-28 13:32:54 -0300

    Update ReciboLocaliz.js

--- Modulos/ReciboLocaliz.js
+++ Modulos/ReciboLocaliz.js
@@ -2,7 +2,7 @@ export function ReciboLocaliz() {
 
     // Verifica se o link está vazio
     const print = document.getElementById('print');
-    print.style.height = '900px'; // Altera a altura da div para 900px
+    print.style.height = '850px'; // Altera a altura da div para 850px
 
     // Obtém referências aos elementos HTML
     const wrapper = document.querySelector(".wrapper"),
